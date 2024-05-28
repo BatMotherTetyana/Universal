c111e1a694cd0140e790e6ac968460091d2061a8
Limbo <bat.girl.bts@gmail.com>
2024-05-28 15:41:28 +0300

    ex11

---
+++ resources/views/nav.blade.php
@@ -0,0 +1,15 @@
+<nav>
+    <ul>
+        <li>
+            <a href="{{ route('home') }}">Головна</a>
+        </li>
+
+        <li>
+            <a href="{{ route('contacts') }}">Контакти</a>
+        </li>
+
+        <li>
+            <a href="{{ route('about') }}">Інфо</a>
+        </li>
+    </ul>
+</nav>
